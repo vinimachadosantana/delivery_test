0abae20a89891e480bf400bf83e58e6fb1ef7512
Vini Machado <vinimachadosantana@gmail.com>
2021-05-28 19:37:12 -0300

    Refac order specs

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -5,10 +5,12 @@ require "rails_helper"
 RSpec.describe Order, type: :model do
   describe "associations" do
     it { is_expected.to have_one(:customer) }
+    it { is_expected.to have_one(:address) }
     it { is_expected.to have_many(:items) }
     it { is_expected.to have_many(:payments) }
 
     it { is_expected.to accept_nested_attributes_for(:customer) }
+    it { is_expected.to accept_nested_attributes_for(:address) }
     it { is_expected.to accept_nested_attributes_for(:items) }
     it { is_expected.to accept_nested_attributes_for(:payments) }
   end
@@ -19,13 +21,6 @@ RSpec.describe Order, type: :model do
     it { is_expected.to validate_presence_of(:sub_total) }
     it { is_expected.to validate_presence_of(:delivery_fee) }
     it { is_expected.to validate_presence_of(:total) }
-    it { is_expected.to validate_presence_of(:country) }
-    it { is_expected.to validate_presence_of(:state) }
-    it { is_expected.to validate_presence_of(:city) }
-    it { is_expected.to validate_presence_of(:district) }
-    it { is_expected.to validate_presence_of(:street) }
-    it { is_expected.to validate_presence_of(:latitude) }
-    it { is_expected.to validate_presence_of(:longitude) }
     it { is_expected.to validate_presence_of(:dt_order_create) }
     it { is_expected.to validate_presence_of(:postal_code) }
     it { is_expected.to validate_presence_of(:number) }
