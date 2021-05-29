1321b6d3df2f7132515c6ad505f4923075a61903
Vini Machado <vinimachadosantana@gmail.com>
2021-05-29 00:25:24 -0300

    Add generate token service

---
+++ app/services/generate_token_service.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class GenerateTokenService < BaseService
+  TOKEN_URL = URI(ENV["API_URL_TOKEN"]).freeze
+
+  def call
+    JSON.parse(request.body)['token'].chomp
+  end
+
+  private
+
+  def request
+    connection.request(get)
+  end
+
+  def connection
+    http = Net::HTTP.new(TOKEN_URL.host, TOKEN_URL.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http
+  end
+
+  def get
+    req = Net::HTTP::Get.new(TOKEN_URL.request_uri)
+    req
+  end
+end
