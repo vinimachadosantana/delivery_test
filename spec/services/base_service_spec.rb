6063ee070e2137b6f50379a12bf4b44e2b80e207
Vini Machado <vinimachadosantana@gmail.com>
2021-05-29 00:24:18 -0300

    Add base service specs

---
+++ spec/services/base_service_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe BaseService do
+  describe '.call' do
+    it 'delegates the call to a new instance' do
+      arguments = double('service call arguments')
+      service = instance_spy(described_class)
+      allow(described_class).to receive(:new).and_return(service)
+
+      described_class.call(arguments)
+
+      expect(service).to have_received(:call).with(arguments)
+    end
+
+    it 'delegates the call to a new instance when called with bang' do
+      arguments = double('service call arguments')
+      service = instance_spy(described_class)
+      allow(described_class).to receive(:new).and_return(service)
+
+      described_class.call!(arguments)
+
+      expect(service).to have_received(:call!).with(arguments)
+    end
+  end
+
+  describe '#call' do
+    it 'raises a NotImplementedError' do
+      subject = described_class.new
+
+      expect { subject.call }.to raise_error(NotImplementedError)
+    end
+  end
+
+  describe '#call!' do
+    it 'raises a NotImplementedError' do
+      subject = described_class.new
+
+      expect { subject.call! }.to raise_error(NotImplementedError)
+    end
+  end
+end
