76947a746cb3a069ba8934222fa6b5a2defdc95a
Vini Machado <vinimachadosantana@gmail.com>
2021-05-29 00:25:56 -0300

    Add token to request header

--- app/services/process_order_service.rb
+++ app/services/process_order_service.rb
@@ -3,6 +3,10 @@
 class ProcessOrderService < BaseService
   URL = URI(ENV["API_URL"]).freeze
 
+  def initialize(get_token: GenerateTokenService)
+    @get_token = get_token.call
+  end
+
   def call(payload)
     response = connection.request(request(payload))
 
@@ -15,6 +19,7 @@ class ProcessOrderService < BaseService
     req = Net::HTTP::Post.new(URL.path)
     req["Content-Type"] = "application/json"
     req["X-Sent"] = Time.zone.now.strftime("%Hh%M - %d/%m/%y")
+    req["Authorization"] = @get_token
     req.body = body
     req
   end
