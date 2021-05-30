93e51c0e6b5ed0de1138f64b9042397739638076
Vini Machado <vinimachadosantana@gmail.com>
2021-05-30 14:41:23 -0300

    Reach 100% coverage

--- spec/requests/api/v1/orders_controller_spec.rb
+++ spec/requests/api/v1/orders_controller_spec.rb
@@ -15,27 +15,27 @@ describe Api::V1::OrdersController, type: :request do
         post api_v1_orders_path, params: params
       end
 
-      it 'respond with 202 status accepted' do
+      it 'responds with 202 status accepted' do
         expect(response).to have_http_status(:accepted)
       end
 
-      it 'create new order' do
+      it 'creates new order' do
         expect(Order.count).to eq(1)
       end
 
-      it 'create new order items' do
+      it 'creates new order items' do
         expect(Item.count).to eq(1)
       end
 
-      it 'create new customer' do
+      it 'creates new customer' do
         expect(Customer.count).to eq(1)
       end
 
-      it 'create new payment' do
+      it 'creates new payment' do
         expect(Payment.count).to eq(1)
       end
 
-      it 'create new address' do
+      it 'creates new address' do
         expect(Address.count).to eq(1)
       end
     end
@@ -45,11 +45,11 @@ describe Api::V1::OrdersController, type: :request do
 
       before { post api_v1_orders_path, params: params }
 
-      it 'respond with 404 status' do
+      it 'responds with 404 status' do
         expect(response).to have_http_status(:not_found)
       end
 
-      it 'doesnt persists order' do
+      it 'doesnt persist order' do
         expect(Order.count).to eq(0)
       end
     end
@@ -63,8 +63,18 @@ describe Api::V1::OrdersController, type: :request do
         post api_v1_orders_path, params: params
       end
 
-      it 'respond with status service unavailable' do
+      it 'responds with status service unavailable' do
         expect(response).to have_http_status(:not_found)
+      end
+
+      it 'responds with rescue values' do
+        controller = Api::V1::OrdersController.new
+
+        expect(controller.send(:country)).to eq('BR')
+        expect(controller.send(:address_block)).to eq({})
+        expect(controller.send(:customer_block)).to eq({})
+        expect(controller.send(:items_block)).to eq([])
+        expect(controller.send(:payments_block)).to eq([])
       end
     end
   end
